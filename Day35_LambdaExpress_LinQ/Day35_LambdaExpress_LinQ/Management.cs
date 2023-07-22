c68e4d0a370641742adf8fe0e8f33923db50dedd
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-22 01:14:47 -0400

    [Ruchita]Add:retrive top 3 records using a Lambda Expression

---
+++ Day35_LambdaExpress_LinQ/Day35_LambdaExpress_LinQ/Management.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day35_LambdaExpress_LinQ
+{
+    public class Management
+    {
+        //UC2:-Retrieve top 3 records from the list who’s rating is high using Lambda 
+        public void TopRecord(List<ProductReview> products)
+        {
+
+            var result = products.OrderByDescending(x => x.Rating).Take(3);
+            Console.WriteLine("Using lambda");
+            Console.WriteLine("-------------------------------");
+            foreach (var prod in result)
+            {
+                Console.WriteLine(prod.ProductID + "  " + prod.UserID + "  " + prod.Rating + "  " + prod.Review + "  " + prod.IsLike);
+
+            }
+        }
+    }
+}
--- Day35_LambdaExpress_LinQ/Day35_LambdaExpress_LinQ/Program.cs
+++ Day35_LambdaExpress_LinQ/Day35_LambdaExpress_LinQ/Program.cs
@@ -11,6 +11,8 @@ namespace Day35_LambdaExpress_LinQ
         static void Main(string[] args)
             {
             //Create variable for List of Product Review class in Main method- Add 25 default values in list which we have created
+
+            Management management = new Management();   
                 List<ProductReview> list = new List<ProductReview>()
             {
                 new ProductReview(){ ProductID = 1,UserID = 1, Rating=5, Review="Average",IsLike=true},
@@ -22,26 +24,38 @@ namespace Day35_LambdaExpress_LinQ
                 new ProductReview(){ ProductID = 7,UserID = 7, Rating=10, Review="VeryGood",IsLike=true},
                 new ProductReview(){ ProductID = 8,UserID = 8, Rating=7, Review="Nice",IsLike=true},
                 new ProductReview(){ ProductID = 9,UserID = 9, Rating=5, Review="Average",IsLike=false},
-                new ProductReview(){ ProductID = 10,UserID = 10, Rating=4, Review="Average",IsLike=false},
-                new ProductReview(){ ProductID = 11,UserID = 11, Rating=9, Review="Good",IsLike=true},
-                new ProductReview(){ ProductID = 12,UserID = 12, Rating=1, Review="Bad",IsLike=false},
-                new ProductReview(){ ProductID = 1,UserID = 13, Rating=3, Review="Bad",IsLike=false},
-                new ProductReview(){ ProductID = 14,UserID = 14, Rating=9, Review="Good",IsLike=true},
-                new ProductReview(){ ProductID = 15,UserID = 15, Rating=6, Review="Nice",IsLike=true},
-                new ProductReview(){ ProductID = 16,UserID = 16, Rating=10, Review="VeryGood",IsLike=false},
-                new ProductReview(){ ProductID = 17,UserID = 17, Rating=4, Review="Bad",IsLike=true},
-                new ProductReview(){ ProductID = 18,UserID = 18, Rating=6, Review="Nice",IsLike=false},
-                new ProductReview(){ ProductID = 19,UserID = 19, Rating=8, Review="Good",IsLike=false},
-                new ProductReview(){ ProductID = 20,UserID = 20, Rating=2, Review="Bad",IsLike=true},
-                new ProductReview(){ ProductID = 21,UserID = 21, Rating=1, Review="Bad",IsLike=false},
-                new ProductReview(){ ProductID = 6,UserID = 22, Rating=9, Review="Good",IsLike=true},
-                new ProductReview(){ ProductID = 4,UserID = 23, Rating=5, Review="Average",IsLike=false},
-                new ProductReview(){ ProductID = 17,UserID = 24, Rating=10, Review="VeryGood",IsLike=false},
-                new ProductReview(){ ProductID = 6,UserID = 25, Rating=6, Review="Nice",IsLike=false},
+                new ProductReview(){ ProductID = 10,UserID = 10,Rating=4, Review="Average",IsLike=false},
+                new ProductReview(){ ProductID = 11,UserID = 11,Rating=9, Review="Good",IsLike=true},
+                new ProductReview(){ ProductID = 12,UserID = 12,Rating=1, Review="Bad",IsLike=false},
+                new ProductReview(){ ProductID = 1,UserID = 13,Rating=3, Review="Bad",IsLike=false},
+                new ProductReview(){ ProductID = 14,UserID = 14,Rating=9, Review="Good",IsLike=true},
+                new ProductReview(){ ProductID = 15,UserID = 15,Rating=6, Review="Nice",IsLike=true},
+                new ProductReview(){ ProductID = 16,UserID = 16,Rating=10,Review="VeryGood",IsLike=false},
+                new ProductReview(){ ProductID = 17,UserID = 17,Rating=4, Review="Bad",IsLike=true},
+                new ProductReview(){ ProductID = 18,UserID = 18,Rating=6, Review="Nice",IsLike=false},
+                new ProductReview(){ ProductID = 19,UserID = 19,Rating=8, Review="Good",IsLike=false},
+                new ProductReview(){ ProductID = 20,UserID = 20,Rating=2, Review="Bad",IsLike=true},
+                new ProductReview(){ ProductID = 21,UserID = 21,Rating=1, Review="Bad",IsLike=false},
+                new ProductReview(){ ProductID = 6,UserID = 22,Rating=9, Review="Good",IsLike=true},
+                new ProductReview(){ ProductID = 4,UserID = 23,Rating=5, Review="Average",IsLike=false},
+                new ProductReview(){ ProductID =17,UserID=  24,Rating=10,Review="VeryGood",IsLike=false},
+                new ProductReview(){ ProductID = 6,UserID = 25,Rating=6, Review="Nice",IsLike=false},
 
             };
-
+            while (true)
+            {
+                Console.WriteLine("1-Top three ratings");
+                Console.WriteLine("Choose a option");
+                int op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        management.TopRecord(list);
+                        break;
+                }
             }
+
+        }
         }
     }
 
